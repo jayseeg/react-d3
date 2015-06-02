5731c02ff1b1d44eff59eaa9bc104639625518c0
Jake Gibbons <jgibbons@placeable.com>
2015-06-01 18:37:54 -0600

    Added sloppy support for supporting legends on pie chart.

--- src/piechart/PieChart.jsx
+++ src/piechart/PieChart.jsx
@@ -46,7 +46,7 @@ module.exports = React.createClass({
   render: function() {
     var props = this.props;
 
-    var transform = `translate(${ props.cx || props.width/2 },${ props.cy || props.height/2 })`;
+    var transform = `translate(${ props.radius },${ props.radius })`;
 
     var values = props.data.map( (item) => item.value );
     var labels = props.data.map( (item) => item.label );
